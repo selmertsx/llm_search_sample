62d17dd4e60ce5bfc70311c12c8f7d25c61d53a7
selmertsx <selmertsx@gmail.com>
2025-05-11 13:07:05 +0900

    検索フォームを作った

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1>DEMO!!</h1>
-      </main>
-    </div>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5" gutterBottom>
+          OSS ライブラリ情報検索
+        </Typography>
+        <TextField
+          id="outlined-basic"
+          label="ライブラリ名を入力"
+          variant="outlined"
+          fullWidth
+          sx={{ mt: 2 }}
+        />
+      </Box>
+    </Container>
   );
 }
