3adc469dc0f7c87ff5e858c519392cb9819a948f
selmertsx <selmertsx@gmail.com>
2025-05-11 11:57:54 +0900

    add zod package

--- scripts/search_with_mastra.ts
+++ scripts/search_with_mastra.ts
@@ -3,18 +3,19 @@ import { google } from "@ai-sdk/google";
 
 async function main() {
   const agent = new Agent({
-    name: "NewsSearchAgent",
-    instructions: "You are a helpful AI assistant that searches for news.",
+    name: "PeasonSearchAgent",
+    instructions: "You are a helpful AI assistant that searches for person.",
     model: google("gemini-2.5-pro-preview-05-06", {
       useSearchGrounding: true,
     }),
   });
 
   const prompt =
-    "List the top 5 San Francisco news from the past week." +
-    "You must include the date of each article.";
+    "github accountのaccount名がselmertsxという人がいます。この人の本名と所属会社、所属部署を教えてください";
 
-  const result = await agent.generate(prompt);
+  const result = await agent.generate(
+    prompt,
+  );
   console.log(result);
 }
 
