0f61ff106d6303c67d0c9a2d613dd79cc0471b5e
selmertsx <selmertsx@gmail.com>
2025-05-11 12:03:48 +0900

    構造化した出力を返すようにした

--- scripts/search_with_mastra.ts
+++ scripts/search_with_mastra.ts
@@ -1,5 +1,13 @@
 import { Agent } from "@mastra/core/agent";
 import { google } from "@ai-sdk/google";
+import { Message } from "@ai-sdk/ui-utils";
+import { z } from "zod";
+
+const PersonInfoSchema = z.object({
+  fullName: z.string().describe("本名"),
+  company: z.string().describe("所属会社"),
+  department: z.string().describe("所属部署"),
+});
 
 async function main() {
   const agent = new Agent({
@@ -10,12 +18,16 @@ async function main() {
     }),
   });
 
-  const prompt =
-    "github accountのaccount名がselmertsxという人がいます。この人の本名と所属会社、所属部署を教えてください";
+  const prompt: Message = {
+    id: "1",
+    role: "user",
+    content:
+      "github accountのaccount名がselmertsxという人がいます。この人の本名と所属会社、所属部署を教えてください",
+  };
 
-  const result = await agent.generate(
-    prompt,
-  );
+  const result = await agent.generate([prompt], {
+    output: PersonInfoSchema,
+  });
   console.log(result);
 }
 
