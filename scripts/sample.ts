541579f51ace9143c7ac69418bd21d38eb026e4f
selmertsx <selmertsx@gmail.com>
2025-05-11 10:46:01 +0900

    generateTextの参考実装

--- scripts/sample.ts
+++ scripts/sample.ts
@@ -1,14 +1,18 @@
 import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
 
-const { text, sources } = await generateText({
-  model: google("gemini-1.5-pro", {
-    useSearchGrounding: true,
-  }),
-  prompt:
-    "List the top 5 San Francisco news from the past week." +
-    "You must include the date of each article.",
-});
+async function main() {
+  const { text, sources } = await generateText({
+    model: google("gemini-2.5-pro-preview-05-06", {
+      useSearchGrounding: true,
+    }),
+    prompt:
+      "List the top 5 San Francisco news from the past week." +
+      "You must include the date of each article.",
+  });
 
-console.log(text);
-console.log(sources);
+  console.log(text);
+  console.log(sources);
+}
+
+main();
